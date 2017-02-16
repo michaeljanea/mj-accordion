9943e5c115f2b795e96ea6ac03d40ed234ae6ad0
Regicide <Regicide>
2017-02-16 13:52:04 +0800

    First commit

---
+++ mjAccordion.js
@@ -0,0 +1,7 @@
+/*
+	Author		: Michael Janea
+	Version		: 1.0
+	Author URL	: www.michaeljanea.byethost7.com
+*/
+
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('(2($){$.s.n=2(a){v b=$.p({5:t,i:l},a);m 0.o(2(){$(0).h(\'.e\').r(\'x\',2(){f(b.i){f(!$(0).u(\'1\')){$(0).d(\'1\').4(\'.9\').7(3,3).g(b.5,2(){$(0).d(\'1\').c(\'6\')})}j{$(0).4(\'.9\').7(3,3).k(b.5,2(){$(0).8(\'1\').c(\'6\').4(\'.e\').8(\'1\')})}}j{$(0).d(\'1\').4(\'.9\').7(3,3).g(b.5,2(){$(0).d(\'1\').c(\'6\')});$(0).w().4().h(\'.9\').7(3,3).k(b.5,2(){$(0).8(\'1\').c(\'6\').4(\'.e\').8(\'1\')})}})})}})(q);',34,34,'this|active|function|true|siblings|duration|style|stop|removeClass|mj_accordion_content|||removeAttr|addClass|mj_accordion_item|if|slideDown|find|toggle|else|slideUp|false|return|mjAccordion|each|extend|jQuery|on|fn|300|hasClass|var|parent|click'.split('|'),0,{}))
